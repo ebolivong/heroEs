f1b73515fe57afe4afdccf54a10f86d78e32aef3
Graphic <graphic@SOD-SJH-516-05M.local>
2015-10-23 13:02:34 -0400

    took up in class

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
 $(document).ready(function(){
+// MY ATTEMPT
+// var $firsthero = $('img:first');
+// var $lasthero = $('img:last');
 
-var $firsthero = $('img:first');
-var $lasthero = $('img:last');
-
-	$firsthero.addClass('shown');
-	$('.name').text('BUTMAN');
-	// $('.name').text$firsthero.attr("alt");
+// 	$firsthero.addClass('shown');
+// 	$('.name').text('BUTMAN');
+// 	// $('.name').text$firsthero.attr("alt");
 
 
-	$('.next').on('click', function() {
-		$firsthero.removeClass('shown');
-		// $.addClass('shown');
-		$('.name').text('CAPTAIN MURICA');
+// 	$('.next').on('click', function() {
+// 		$firsthero.removeClass('shown');
+// 		// $.addClass('shown');
+// 		$('.name').text('CAPTAIN MURICA');
+// 	});
+// }); 
+
+// IN CLASS TAKE UP
+	$('.gallery :first').addClass('shown');
+
+	$('button.next').on('click', function() {
+
+		if ($('.shown').is(':last-child')) {
+			$('.shown').removeClass('shown');
+			$('.gallery :first').addClass('shown');
+		}
+
+		else {
+			$('.shown').removeClass('shown').next().addClass('shown')
+		}
+
+		var caption = $('.shown').attr('title');
+		$('.name').text(caption).show().fadeOut(1000);
+
 	});
-}); 
 
+	$('button.prev').on('click', function() {
 
+		if ($('.shown').is(':first-child')) {
+			$('.shown').removeClass('shown');
+			$('.gallery :last').addClass('shown');
+		}
 
- 
+		else {
+			$('.shown').removeClass('shown').prev().addClass('shown')
+		}
+
+		var caption = $('.shown').attr('title');
+		$('.name').text(caption).show().fadeOut(1000);
+
+	});
+
+});
